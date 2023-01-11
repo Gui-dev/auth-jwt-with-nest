d1745ad000282fc5f3f77ac2816fddb2405c546e
Gui-dev <adrianon10@gmail.com>
2023-01-11 02:21:44 -0300

    feat: add fake database

---
+++ src/utils/users.ts
@@ -0,0 +1,29 @@
+type UserProps = {
+  id: string
+  name: string
+  username: string
+  password: string
+}
+
+export const users: UserProps[] = [
+  {
+    id: '1',
+    name: 'Bruce Wayne',
+    username: 'bruce@email.com',
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+  },
+
+  {
+    id: '2',
+    name: 'Clark Kent',
+    username: 'clark@email.com',
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+  },
+
+  {
+    id: '3',
+    name: 'Barry Allen',
+    username: 'barry@email.com',
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+  }
+]
