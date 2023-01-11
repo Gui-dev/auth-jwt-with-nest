0210a69b10be7bdbe010838028f60cfa497be64d
Gui-dev <adrianon10@gmail.com>
2023-01-11 03:23:26 -0300

    feat: add new property role

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -3,6 +3,7 @@ type UserProps = {
   name: string
   username: string
   password: string
+  role: string
 }
 
 export const users: UserProps[] = [
@@ -10,20 +11,23 @@ export const users: UserProps[] = [
     id: '1',
     name: 'Bruce Wayne',
     username: 'bruce@email.com',
-    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy',
+    role: 'admin'
   },
 
   {
     id: '2',
     name: 'Clark Kent',
     username: 'clark@email.com',
-    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy',
+    role: 'user'
   },
 
   {
     id: '3',
     name: 'Barry Allen',
     username: 'barry@email.com',
-    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy'
+    password: '$2b$10$xB8TETTwxWOsyHAd4Hmydud/GcwCC2482O4N.fyxTl37NYs2XQtVy',
+    role: 'user'
   }
 ]
